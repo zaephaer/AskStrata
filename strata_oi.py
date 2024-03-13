96a4448221db9c1129313dd0d8d298d14008b91b
Mohd Azhar Ahmad <41311744+zaephaer@users.noreply.github.com>
2024-03-13 10:03:43 +0800

    Add files via upload

---
+++ strata_oi.py
@@ -0,0 +1,39 @@
+# Using OpenAI
+# Using text file as knowledge base
+
+import os
+import streamlit as st
+import openai
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+import traceback
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Simple Streamlit app
+st.set_page_config(page_title="Strata Property in Malaysia", page_icon=":house:", layout="wide")
+st.title("Ask about Strata Property in Malaysia")
+query = st.text_input("Enter your question")
+
+# If submit button click
+if st.button("Submit"):
+    if not query.strip():
+        st.error("Please enter a query")
+    else:
+        try:
+            # Define custom LLM Model
+            llm_predictor = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo")
+
+            # Load documents from directory name 'data'
+            documents = SimpleDirectoryReader('data').load_data()
+            service_context = ServiceContext.from_defaults()
+
+            # Build Index
+            index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+            query_engine = index.as_query_engine()
+            response = query_engine.query(query)
+            st.success(response)
+        except Exception as e:
+            st.error(f"Error occurred: {e}")
+            st.error(traceback.format_exc())
